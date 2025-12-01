b6a809254d29c1ceb0c2992c2ce7d9ba86833fb8
dstlmike <17973597+dstlmike@users.noreply.github.com>
2025-12-01 05:41:09 -0500

    Create AnnotationActivity.kt

---
+++ AnnotationActivity.kt
@@ -0,0 +1,18 @@
+private fun exportToPdf(bitmap: Bitmap) {
+    val pdfPath = File(getExternalFilesDir(null), "report.pdf").absolutePath
+    val document = com.itextpdf.text.Document()
+    val writer = com.itextpdf.text.pdf.PdfWriter.getInstance(document, FileOutputStream(pdfPath))
+
+    document.open()
+
+    val stream = ByteArrayOutputStream()
+    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream)
+    val image = com.itextpdf.text.Image.getInstance(stream.toByteArray())
+
+    image.scaleToFit(500f, 700f)
+    document.add(image)
+
+    document.close()
+
+    Toast.makeText(this, "PDF saved at: $pdfPath", Toast.LENGTH_LONG).show()
+}
