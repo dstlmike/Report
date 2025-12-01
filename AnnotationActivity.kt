534e048322b96ea5ecb4de1da4b83be19a77fca8
dstlmike <17973597+dstlmike@users.noreply.github.com>
2025-12-01 05:44:08 -0500

    Implement PDF export functionality from bitmap
    Added exportToPdf function to create a PDF from a bitmap.

--- AnnotationActivity.kt
+++ AnnotationActivity.kt
@@ -17,20 +17,21 @@ private fun exportToPdf(bitmap: Bitmap) {
     Toast.makeText(this, "PDF saved at: $pdfPath", Toast.LENGTH_LONG).show()
 }
 
-<FrameLayout xmlns:android="http://schemas.android.com/apk/res/android"
-    android:layout_width="match_parent"
-    android:layout_height="match_parent">
+private fun exportToPdf(bitmap: Bitmap) {
+    val pdfPath = File(getExternalFilesDir(null), "report.pdf").absolutePath
+    val document = com.itextpdf.text.Document()
+    val writer = com.itextpdf.text.pdf.PdfWriter.getInstance(document, FileOutputStream(pdfPath))
 
-    <androidx.camera.view.PreviewView
-        android:id="@+id/previewView"
-        android:layout_width="match_parent"
-        android:layout_height="match_parent"/>
+    document.open()
 
-    <Button
-        android:id="@+id/btnCapture"
-        android:text="Capture"
-        android:layout_gravity="bottom|center"
-        android:layout_margin="24dp"
-        android:layout_width="wrap_content"
-        android:layout_height="wrap_content"/>
-</FrameLayout>
+    val stream = ByteArrayOutputStream()
+    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream)
+    val image = com.itextpdf.text.Image.getInstance(stream.toByteArray())
+
+    image.scaleToFit(500f, 700f)
+    document.add(image)
+
+    document.close()
+
+    Toast.makeText(this, "PDF saved at: $pdfPath", Toast.LENGTH_LONG).show()
+}
